812e2249f0c5b4ba42990d0662c23c128ae18ad1
luster <ethan.lusterman@gmail.com>
2014-10-15 15:31:27 -0400

    edit

--- js/lastfm.js
+++ js/lastfm.js
@@ -1,13 +1,14 @@
 /*
+ *
  * Ethan Lusterman
  *      lastfm.js
  *      - displays currently playing track or last played track via lastfm api
  *      - appends corresponding html to #lastfm on the page
  *      - please sign up for your own API key if you use my code!
+ *
  */
 
 var url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=guitarkid7427&limit=1&nowplaying=true&api_key=1a7a23392098d573fb78db3fdadd28e7&format=json';
-var x;
 
 function genHtml(track, isNowPlaying) {
     var html;
@@ -34,14 +35,17 @@ $.ajax({
     dataType: 'jsonp',
     success: function(d) {
         var track = d.recenttracks.track;
+
         if (track.length > 1) {
             track = track[0];
         }
+
         if ("\@attr" in track) {
             // song is currently playing
             genHtml(track, true);
             return;
         }
+
         // last song is not currently playing
         genHtml(track, false);
         return;
--- js/nav.js
+++ js/nav.js
@@ -1,26 +1,37 @@
 /*
+ *
  * Ethan Lusterman
  *      nav.js
  *      - toggles which div to display
  *      - adjusts corresponding navigation link style
+ *
  */
 
+// set css for when a nav item is clicked
 var cssText = 'opacity:0.5;cursor:default;';
+
 var dis = document.getElementById('social');
 var navdis = document.getElementById('nav-social');
+
+// set dis/navdis pair to "clicked" and display div
 navdis.style.cssText = cssText;
 dis.style.display = 'block';
 
+// called each time a navigation link is clicked
 function makeVisible(id) {
   var e = document.getElementById(id);
+
   if (e === dis)
     return;
 
   dis.style.display = 'none';
   navdis.style.cssText = 'cursor:pointer;';
+
   dis = e;
+
   navdis = document.getElementById('nav-'+id);
   dis.style.display = 'block';
   navdis.style.cssText = cssText;
+
   return;
 }
